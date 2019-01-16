07b0b094757c4ba1dbf070a51561e6268912caf8
Maddy Weller <madelynweller@gmail.com>
2019-01-16 11:20:31 -0600

    I figured out how to submit a book, but not how to connect my tables, yet.

--- CoderGirl Book Project/Data/Migrations/20190116160445_books.Designer.cs
+++ CoderGirl Book Project/Data/Migrations/20190116160445_books.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CoderGirl_Book_Project.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190116160445_books")]
+    partial class books
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CoderGirl Book Project/Data/Migrations/20190116160445_books.cs
@@ -0,0 +1,312 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CoderGirl_Book_Project.Data.Migrations
+{
+    public partial class books : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Author = table.Column<string>(unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Cover = table.Column<byte[]>(type: "image", nullable: true),
+                    Description = table.Column<string>(unicode: false, nullable: false),
+                    Title = table.Column<string>(unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Category = table.Column<string>(unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ratings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Rating = table.Column<string>(unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ratings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Tag = table.Column<string>(unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Book_Author",
+                columns: table => new
+                {
+                    Book_Id = table.Column<int>(nullable: false),
+                    Author_Id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Book_Author_pk", x => new { x.Book_Id, x.Author_Id });
+                    table.ForeignKey(
+                        name: "FK_Author",
+                        column: x => x.Author_Id,
+                        principalTable: "Author",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Book",
+                        column: x => x.Book_Id,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books_Category",
+                columns: table => new
+                {
+                    Book_Id = table.Column<int>(nullable: false),
+                    Category_Id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Books_Ca__34F8CB6279506F07", x => new { x.Book_Id, x.Category_Id });
+                    table.ForeignKey(
+                        name: "FK__Books_Cat__Book___55BFB948",
+                        column: x => x.Book_Id,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK__Books_Cat__Categ__6F7F8B4B",
+                        column: x => x.Category_Id,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books_Rating",
+                columns: table => new
+                {
+                    Book_Id = table.Column<int>(nullable: false),
+                    Rating_Id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Book_Rating_pk", x => new { x.Book_Id, x.Rating_Id });
+                    table.ForeignKey(
+                        name: "FK__Books_Rat__Book___2116E6DF",
+                        column: x => x.Book_Id,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK__Books_Rat__Ratin__220B0B18",
+                        column: x => x.Rating_Id,
+                        principalTable: "Ratings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books_Tags",
+                columns: table => new
+                {
+                    Book_Id = table.Column<int>(nullable: false),
+                    Tag_Id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Books_Ta__EF29367506627FBC", x => new { x.Book_Id, x.Tag_Id });
+                    table.ForeignKey(
+                        name: "FK__Books_Tag__Book___56B3DD81",
+                        column: x => x.Book_Id,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK__Books_Tag__Tag_I__7073AF84",
+                        column: x => x.Tag_Id,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "AK_Author_Column",
+                table: "Author",
+                column: "Author",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_Author_Author_Id",
+                table: "Book_Author",
+                column: "Author_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Category_Category_Id",
+                table: "Books_Category",
+                column: "Category_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Rating_Rating_Id",
+                table: "Books_Rating",
+                column: "Rating_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Tags_Tag_Id",
+                table: "Books_Tags",
+                column: "Tag_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "AK_Category_Column",
+                table: "Categories",
+                column: "Category",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "AK_Ratings_Column",
+                table: "Ratings",
+                column: "Rating",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "AK_Tag_Column",
+                table: "Tags",
+                column: "Tag",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "Book_Author");
+
+            migrationBuilder.DropTable(
+                name: "Books_Category");
+
+            migrationBuilder.DropTable(
+                name: "Books_Rating");
+
+            migrationBuilder.DropTable(
+                name: "Books_Tags");
+
+            migrationBuilder.DropTable(
+                name: "Author");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Ratings");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName");
+        }
+    }
+}
